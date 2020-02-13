7756c3baae72a7a71d8d3566e95e16fbe05e9d8d
philip-github <philiptwal93@gmail.com>
2020-02-13 12:29:19 -0600

    FirstCommit
    WeatherRxswiftAPI
    Most stable code for now

---
+++ WeatherRxSwiftProject/Model.swift
@@ -0,0 +1,146 @@
+//
+//  Model.swift
+//  WeatherRxSwiftProject
+//
+//  Created by Philip Twal on 2/12/20.
+//  Copyright © 2020 Philip Twal. All rights reserved.
+//
+
+import Foundation
+
+
+//struct WeatherModel : Codable{
+//
+//    var timezone : String?
+//    var currently: CurrentWeather?
+//}
+//
+//
+//struct CurrentWeather: Codable {
+//    var time : Double?
+//    var summary: String?
+//    var icon: String?
+//    var temperature : Double?
+//    var humidity: Double?
+//}
+
+struct Welcome: Codable {
+    let latitude, longitude: Double?
+    let timezone: String?
+    let currently: Currently
+    let minutely: Minutely
+    let hourly: Hourly
+    let daily: Daily
+    let flags: Flags
+    let offset: Int
+}
+
+// MARK: - Currently
+struct Currently: Codable {
+    let time: Int
+    let summary: Summary
+    let icon: Icon
+    let nearestStormDistance: Int?
+    let precipIntensity: Double
+    let precipIntensityError: Int?
+    let precipProbability: Double
+    let precipType: PrecipType?
+    let temperature, apparentTemperature, dewPoint, humidity: Double
+    let pressure, windSpeed, windGust: Double
+    let windBearing: Int
+    let cloudCover: Double
+    let uvIndex: Int
+    let visibility, ozone: Double
+}
+
+enum Icon: String, Codable {
+    case clearNight = "clear-night"
+    case cloudy = "cloudy"
+    case partlyCloudyDay = "partly-cloudy-day"
+    case partlyCloudyNight = "partly-cloudy-night"
+}
+
+enum PrecipType: String, Codable {
+    case rain = "rain"
+}
+
+enum Summary: String, Codable {
+    case clear = "Clear"
+    case mostlyCloudy = "Mostly Cloudy"
+    case overcast = "Overcast"
+    case partlyCloudy = "Partly Cloudy"
+}
+
+// MARK: - Daily
+struct Daily: Codable {
+    let summary, icon: String
+    let data: [DailyDatum]
+}
+
+// MARK: - DailyDatum
+struct DailyDatum: Codable {
+    let time: Int
+    let summary, icon: String
+    let sunriseTime, sunsetTime: Int
+    let moonPhase, precipIntensity, precipIntensityMax: Double
+    let precipIntensityMaxTime: Int
+    let precipProbability: Double
+    let precipType: PrecipType
+    let temperatureHigh: Double
+    let temperatureHighTime: Int
+    let temperatureLow: Double
+    let temperatureLowTime: Int
+    let apparentTemperatureHigh: Double
+    let apparentTemperatureHighTime: Int
+    let apparentTemperatureLow: Double
+    let apparentTemperatureLowTime: Int
+    let dewPoint, humidity, pressure, windSpeed: Double
+    let windGust: Double
+    let windGustTime, windBearing: Int
+    let cloudCover: Double
+    let uvIndex, uvIndexTime: Int
+    let visibility, ozone, temperatureMin: Double
+    let temperatureMinTime: Int
+    let temperatureMax: Double
+    let temperatureMaxTime: Int
+    let apparentTemperatureMin: Double
+    let apparentTemperatureMinTime: Int
+    let apparentTemperatureMax: Double
+    let apparentTemperatureMaxTime: Int
+}
+
+// MARK: - Flags
+struct Flags: Codable {
+    let sources: [String]
+    let nearestStation: Double
+    let units: String
+
+    enum CodingKeys: String, CodingKey {
+        case sources
+        case nearestStation = "nearest-station"
+        case units
+    }
+}
+
+// MARK: - Hourly
+struct Hourly: Codable {
+    let summary: String
+    let icon: Icon
+    let data: [Currently]
+}
+
+// MARK: - Minutely
+struct Minutely: Codable {
+    let summary: String
+    let icon: Icon
+    let data: [MinutelyDatum]
+}
+
+// MARK: - MinutelyDatum
+struct MinutelyDatum: Codable {
+    let time: Int
+    let precipIntensity: Double
+    let precipIntensityError: Double?
+    let precipProbability: Double
+    let precipType: PrecipType?
+}
--- WeatherRxSwiftProject/ViewController.swift
+++ WeatherRxSwiftProject/ViewController.swift
@@ -7,14 +7,89 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
+
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tblView: UITableView!
+    let urlFormat = "https://api.darksky.net/forecast/02b2b82d0bcf56f5ea7db14618cdfa80/37.8267,-122.4233"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpSubsciption()
+        setUpTableViewBind()
     }
+    
+    let disposeBag = DisposeBag()
+    
+    var dataSource : BehaviorRelay<[Welcome]> = BehaviorRelay(value: [])
+    
+    
+    func setUpTableViewBind(){
+        
+        dataSource.bind(to: self.tblView.rx.items) { (tableView, row, element) in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+            let currentTime = element.minutely.data
+            cell.textLabel?.text = "\(String(describing: currentTime)) @row \(row)"
+            
+            return cell
+        }.disposed(by: disposeBag)
+    }
+    
+    
+    func setUpSubsciption(){
+        
+        callAPIFromApiHandler(withUrlString: urlFormat).subscribe(onNext: { (data) in
+            
+            if let myData = data{
+                
+                do{
+                    let welcome = try JSONDecoder().decode(Welcome.self, from: myData)
+                    self.dataSource.accept([welcome])
+                }catch{
+                    print(error.localizedDescription)
+                }
+            }
+        }, onError: { (error) in
+            print(error)
+        }, onCompleted: {
+            DispatchQueue.main.async {
+                self.tblView.reloadData()
+            }
+        }) {
+            print("Disposed")
+        }.disposed(by: disposeBag)
+    
+    }
+    
+    
+     func callAPIFromApiHandler(withUrlString : String)
+        -> Observable<Data?> {
+            //2
+            // observable's create method returns the observer
+            // observer.oncompleted calls when the data is done receiving
+            Observable<Data?>.create { observer  in
+                
+                //3
+                URLSession.shared.dataTask(with: URL(string: withUrlString)!) { (data, response, error) in
+                    
+                    observer.onNext(data)
 
-
+                    if error != nil {
+                        observer.onError(error!)
+                    }
+                    
+                    observer.onCompleted()
+                    
+                }.resume()
+                
+                let disposable = Disposables.create()
+                return disposable
+                
+            }
+    }
 }
 
